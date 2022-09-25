d7891242cd2328385e0d16cbb9257d46282c9f24
Moong <moongstory@gmail.com>
2022-09-26 00:13:54 +0900

    page 113

--- Study.py
+++ Study.py
@@ -1,7 +1,39 @@
-# 85 page
+# page 113
 
-a = [1, 2, 3, 1, 2, 3]
+print("Q1")
+score = {"국어" : 80, "영어" : 75, "수학" : 55}
+total = sum(score.values())
 
-print(a.extend([4, 5]))
+print(total / len(score.values()))
 
-print(a)
+
+
+print("\nQ2")
+num = 13
+if(num % 2 == 0):
+	print("짝수")
+else:
+	print("홀수")
+
+
+
+print("\nQ3")
+pin = "881120-1068234"
+
+yyyymmdd = pin[:pin.find("-")]
+num = pin[pin.find("-") + 1:]
+
+print(yyyymmdd)
+print(num)
+
+
+
+print("\nQ4")
+if(int(pin[pin.find("-") + 1]) % 2 == 0):
+	print("여자")
+else:
+	print("남자")
+
+
+
+print("\nQ5")
