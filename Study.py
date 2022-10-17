1eeb4430f1038650671cbeff5715fbe20808b1f7
Moong <moongstory@gmail.com>
2022-10-17 23:44:33 +0900

    Page 181

--- Study.py
+++ Study.py
@@ -1,39 +1,7 @@
-# page 113
+with open("test.txt", "r") as f:
+	body = f.read()
 
-print("Q1")
-score = {"국어" : 80, "영어" : 75, "수학" : 55}
-total = sum(score.values())
+body = body.replace("java", "python")
 
-print(total / len(score.values()))
-
-
-
-print("\nQ2")
-num = 13
-if(num % 2 == 0):
-	print("짝수")
-else:
-	print("홀수")
-
-
-
-print("\nQ3")
-pin = "881120-1068234"
-
-yyyymmdd = pin[:pin.find("-")]
-num = pin[pin.find("-") + 1:]
-
-print(yyyymmdd)
-print(num)
-
-
-
-print("\nQ4")
-if(int(pin[pin.find("-") + 1]) % 2 == 0):
-	print("여자")
-else:
-	print("남자")
-
-
-
-print("\nQ5")
+with open("test.txt", "w") as f:
+	f.write(body)
